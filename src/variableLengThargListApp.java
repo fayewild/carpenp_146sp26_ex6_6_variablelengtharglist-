140b6eb6d0cafe2941527c120dc2c4a6f603a415
Faye Carpenter <carpenp@email.uscb.edu>
2026-02-15 21:41:55 -0500

    Psudeocode finished.

--- src/variableLengThargListApp.java
+++ src/variableLengThargListApp.java
@@ -3,12 +3,26 @@
  * @author patrickcarpenter
  */
 public class variableLengThargListApp {
-    //
+    // A litte confused on what this one entails, but I'll try!
     
     /*Init. Phase*/
+    // Create ArrayList with no characters that is composed of int values
+    // Create Input variable 
+    // Create InputAmmount variable and set it to 0
+    // Create Total variable for later use
     
     /*Processing Phase*/
+    // Solicit user input
+    // While (user input != -1)
+        // Set Input = user input
+        // Arraylist.add(Input)
+        // ++InputAmmount
+
+    // Product method
+        // For (;InputAmmount != 0;)
+            // Total *= ArrayList.get(0+InputAmmount)
+            // --InputAmmount
     
     /*Termination Phase*/
-    
+    // Print result
 }
